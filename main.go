20cfe12d4cd5738442602eab1ade2bfccdc93844
benzsuankularb <benzsk130@gmail.com>
2015-02-20 14:09:11 +0700

    add test2

--- main.go
+++ main.go
@@ -22,9 +22,24 @@ func main() {
 func Method1() {
 	a := StructA{1, "Created", nil}
 	b := ChangeStructA_Pointer(&a)
-	fmt.Println("VarA,Before is Equal to After", a.VarA == b.VarA)
-	fmt.Println("VarB,Before is Equal to After", a.VarB == b.VarB)
-	fmt.Println("VarC,After is not nil", a.VarC != nil)
+	fmt.Println("VarA,Before is Equal to After :", a.VarA == b.VarA)
+	fmt.Println("VarB,Before is Equal to After :", a.VarB == b.VarB)
+	fmt.Println("VarC,After is not nil :", a.VarC != nil)
+}
+
+func Method2() {
+	a := StructA{1, "Created", nil}
+	b := ChangeStructA(a)
+	fmt.Println("VarA,Before is Equal to After :", a.VarA == b.VarA)
+	fmt.Println("VarB,Before is Equal to After :", a.VarB == b.VarB)
+	fmt.Println("VarC,After is not nil :", a.VarC != nil)
+}
+
+func Method3() {
+	a := StructA{1, "Created", nil}
+	b := &StructA{1, "Created", nil}
+	a = ChangeStructA_Pointer(&a)
+	b = ChangeStructA_Pointer_ReturnPointer(b)
 }
 
 func ChangeStructA_Pointer(r *StructA) StructA {
@@ -32,6 +47,13 @@ func ChangeStructA_Pointer(r *StructA) StructA {
 	r.VarB = "Changed"
 	r.VarC = errors.New("Error Changed.")
 	return *r
+}
+
+func ChangeStructA_Pointer_ReturnPointer(r *StructA) *StructA {
+	r.VarA = 2
+	r.VarB = "Changed"
+	r.VarC = errors.New("Error Changed.")
+	return r
 }
 
 func ChangeStructA(r StructA) StructA {
--- main_test.go
+++ main_test.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestMethod1(t *testing.T) {
+	PrintStart()
 	Method1()
+}
+
+func TestMethod2(t *testing.T) {
+	PrintStart()
+	Method2()
+}
+
+func TestMethod3(t *testing.T) {
+	PrintStart()
+	Method3()
+}
+
+func PrintStart() {
+	fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 }
