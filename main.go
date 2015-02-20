ffc450eff8a6239f33e54d6684dc31052e95440d
benzsuankularb <benzsk130@gmail.com>
2015-02-21 00:47:29 +0700

    add method3-6

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 type StructA struct {
 	VarA int
 	VarB string
-	VarC error
+	VarC error // A pointer of error
 }
 
 type StructB struct {
 	VarA StructA
+	VarB *StructA
 }
 
 func main() {
-
 }
 
 func Method1() {
@@ -40,6 +40,49 @@ func Method3() {
 	b := &StructA{1, "Created", nil}
 	a = ChangeStructA_Pointer(&a)
 	b = ChangeStructA_Pointer_ReturnPointer(b)
+	str1 := fmt.Sprintf("%p", &a)
+	str2 := fmt.Sprintf("%p", b)
+	fmt.Println("Address of A and B is Equal :", str1 == str2)
+}
+
+func Method4() {
+	a := &StructA{1, "Created", nil}
+	addressOfPointer := fmt.Sprintf("%p", &a)
+	pointer := fmt.Sprintf("%p", a)
+	objectA := fmt.Sprint("%p", *a)
+	fmt.Println("Address Of Pointer Of A :", addressOfPointer)
+	fmt.Println("Pointer Point To Address :", pointer)
+	fmt.Println("Object Of Address That Pointer Point To", objectA)
+}
+
+func Method5() {
+	a := &StructA{1, "Created", errors.New("Created Error")}
+	b := Copy(a)
+	fmt.Println("Address A And B is difference :", fmt.Sprintf("%p", a), fmt.Sprintf("%p", &b))
+	fmt.Println("VarB Address of A and B is difference :", fmt.Sprintf("%p", &a.VarB), fmt.Sprintf("%p", &b.VarB))
+	fmt.Println("VarC Address of A and B is difference :", fmt.Sprintf("%p", &a.VarC), fmt.Sprintf("%p", &b.VarC))
+	fmt.Println("VarC of A and B point to same address :", fmt.Sprintf("%p", a.VarC), fmt.Sprintf("%p", b.VarC))
+}
+
+func Method6() {
+	a := StructA{1, "Created", errors.New("Created Error")}
+	b1 := StructB{ a, &a }
+	b2 := CopyB(&b1)
+	fmt.Println("Address StructB B1 And B2 is difference :", fmt.Sprintf("%p", &b1), fmt.Sprintf("%p", &b2))
+	fmt.Println("Address VarB in VarA of B1 And B2 is difference :", fmt.Sprintf("%p", &b1.VarA.VarB), fmt.Sprintf("%p", &b2.VarA.VarB))
+	fmt.Println("Address VarB of B1 And B2 is difference :", fmt.Sprintf("%p", &b1.VarB), fmt.Sprintf("%p", &b2.VarB))
+	fmt.Println("Address that VarB of B1 and B2 pointed is same :", fmt.Sprintf("%p", b1.VarB), fmt.Sprintf("%p", b2.VarB))
+}
+
+func CopyB(r *StructB) StructB {
+	return *r
+}
+
+func Copy(r *StructA) StructA {
+	// Notice
+	// r is pointer point to an address.
+	// *r is object that address holding.
+	return *r
 }
 
 func ChangeStructA_Pointer(r *StructA) StructA {
--- main_test.go
+++ main_test.go
@@ -20,6 +20,21 @@ func TestMethod3(t *testing.T) {
 	Method3()
 }
 
+func TestMethod4(t *testing.T) {
+	PrintStart()
+	Method4()
+}
+
+func TestMethod5(t *testing.T) {
+	PrintStart()
+	Method5()
+}
+
+func TestMethod6(t *testing.T) {
+	PrintStart()
+	Method6()
+}
+
 func PrintStart() {
 	fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 }
